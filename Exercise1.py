39258227b86bc2d2759d6616806bceaa745e44a6
Edwin <edwinsiregar1221@student.uns.ac.id>
2023-03-16 14:05:33 +0700

    Menambahkan Exercise1

---
+++ Exercise1.py
@@ -0,0 +1,5 @@
+print("Edwin Laurensius Alexandro Siregar")
+print("I0322040")
+print("Pematangsiantar")
+print("Di lingkungan sekitar saya terdapat banyak tempat kos untuk anak SMA sehingga jarang terlihat anak-anak yang bermain di komplek rumah. Ada juga lapangan tempat untuk olahraga pagi maupun sore, seperti jogging, pull up, bersepeda, dan lain")
+print("Harapan saya setelah lulus Teknik Industri UNS adalah mendapat pengetahuan yang luas untuk modal masa depan")
